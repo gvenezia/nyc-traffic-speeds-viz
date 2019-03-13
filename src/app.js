1422359817e73a36bc457ccbb75f6169d4123c42
Gaetano <g.venezia@outlook.com>
2019-03-13 13:13:35 -0500

    Configure for new data point positions and display

--- src/app.js
+++ src/app.js
@@ -2,14 +2,6 @@ import * as d3 from 'd3';
 
 import { mapStyles } from './google-map-styles.js';
 
-const latLng = [
-  [40.762901, -74.009763],
-  [40.727711, -74.022358],
-  [40.700838, -74.015411],
-  [40.745058, -73.963737],
-  [40.796309, -73.928990]
-]
-
 // Create the Google Map…
 var map = new google.maps.Map(d3.select("#map").node(), {
   zoom: 11,
@@ -20,10 +12,17 @@ var map = new google.maps.Map(d3.select("#map").node(), {
 });
 
 // Load the station data. When the data comes back, create an overlay.
-d3.csv("data/top5redo-add-type-f.csv", function(error, data) {
+d3.csv("data/DOT_Traffic_Speeds_NBE_limit_1000_f1.csv", function(error, data) {
   if (error) throw error;
 
   var overlay = new google.maps.OverlayView();
+
+  data.forEach( d => {
+    d.lat = d.link_points.match(/[.\d]+/)[0];
+    d.lng = d.link_points.match( /(?<=,)[,\-.\d]+/ )[0];
+
+    console.log(d.lat);
+  })
 
   // Add the container when the overlay is added to the map.
   overlay.onAdd = function() {
@@ -34,10 +33,10 @@ d3.csv("data/top5redo-add-type-f.csv", function(error, data) {
     // Draw each marker as a separate SVG element
     overlay.draw = function() {
       var projection = this.getProjection(),
-          padding = 10;
+          padding = 100;
 
       var marker = layer.selectAll("svg")
-                          .data(d3.entries(data))
+                          .data(data)
                           .each(transform) // update existing markers
                         .enter().append("svg")
                           .each(transform)
@@ -56,8 +55,8 @@ d3.csv("data/top5redo-add-type-f.csv", function(error, data) {
           .attr("dy", ".31em")
           .text(function(d) { return d.key; });
 
-      function transform(d, i) {
-        d = new google.maps.LatLng(latLng[1][0], latLng[1][1]);
+      function transform(d) {
+        d = new google.maps.LatLng( d.lat, d.lng );
         d = projection.fromLatLngToDivPixel(d);
 
         return d3.select(this)
